046e6c603a8268b9bc6d7482545e1da19079c0c4
learn2xpertnakrr <nakhaterohantech@gmail.com>
2023-07-23 20:00:17 +0530

    Tested

---
+++ HelloWorld/Helloworld.py
@@ -0,0 +1,13 @@
+def hello_world(name):
+    """Function Purpose :: Add Hello as prefix in front of name """
+
+    print(f"HELLO {name} GOOD MORNING !!!")
+
+
+if __name__ == '__main__':
+    print("-----Program starts from this line -----")
+
+    name = str(input("Please Enter your name ::"))
+
+    # Function call
+    hello_world(name)
